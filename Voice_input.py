f68bc8ca9b28e652683adb5812e330d4b56eca13
Shivakrishnareddy-03 <shivakrishnareddy2003@gmail.com>
2025-04-10 15:52:55 +0530

    Intial commit :Smart Calc. working version

--- Voice_input.py
+++ Voice_input.py
@@ -1,24 +1,18 @@
-print("Script Started...")  # Add this line at the top
-
 import speech_recognition as sr
 
 def recognize_speech():
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
-        print("🎙️ Listening...")
-        recognizer.adjust_for_ambient_noise(source)
+        print("🎙️ Listening....")
         audio = recognizer.listen(source)
-
-        try:
-            text = recognizer.recognize_google(audio)
-            print("You said:", text)
-            return text
-        except sr.UnknownValueError:
-            print("Sorry, couldn't understand what you said 😕")
-            return None
-        except sr.RequestError:
-            print("Error connecting to speech recognition server")
-            return None
-
-# Call the function
-recognize_speech()
+        print("🎤 Recognizing....")
+    try:
+        text = recognizer.recognize_google(audio, language='en-US')
+        print(f"🗣️ You said: {text}")
+        return text
+    except sr.UnknownValueError:
+        print("🤷 Sorry, I did not understand that.")
+        return None
+    except sr.RequestError:
+        print("🤖 Sorry, there was an error with the speech recognition service.")
+        return None
--- main.py
+++ main.py
@@ -7,6 +7,47 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+def calculate(operation, numbers):
+    if operation == "add":
+        return sum(numbers)
+    elif operation == "subtract":
+        return numbers[0] - numbers[1]
+    elif operation == "multiply":
+        return numbers[0] * numbers[1]
+    elif operation == "divide":
+        try:
+            return numbers[0] / numbers[1]
+        except ZeroDivisionError:
+            return "Cannot divide by Zero"
+    else:
+        return "Operation not recognized"
+    
+def main():
+    print("Script Started...")
+    user_input = recognize_speech()
+    if user_input:
+        result =process_text(user_input)
+        print("Result : ", result)
+
+        if result and result["operation"] and result["numbers"]:
+            final_answer = calculate(result["operation"],result["numbers"])
+            print(f"final Answer : {final_answer}")
+            speak(f"The final answer is {final_answer}")
+        else:
+            speak("Sorry, I couldn't understand the operation or numbers...")
+
+    else:
+        print("I didn't get that")
+        speak("Sorry, I couldn't hear you...")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
 def main():
     print("Script Started...")
     user_input = recognize_speech()
